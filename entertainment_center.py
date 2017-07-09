6a5ea55d12e8655bf55991ed7d186fd4a122ebe4
Vishesh <vmath89@gmail.com>
2017-07-09 14:14:29 +0200

    formatting the code and adding comments

--- entertainment_center.py
+++ entertainment_center.py
@@ -3,33 +3,41 @@ import media
 
 # This file contains the various movies and their individual information.
 
-toy_story = media.Movie('Toy Story', 'A movie about toys coming to life.',
+toy_story = media.Movie('Toy Story',
+                        'A movie about toys coming to life.',
                         'https://a.dilcdn.com/bl/wp-content/uploads/sites/8/2013/02/toy_story_wallpaper_by_artifypics-d5gss19.jpg',
                         'https://www.youtube.com/watch?v=KYz2wyBy3kc')
 
-ice_age = media.Movie('Ice Age', 'A movie about animals protecting a child',
+ice_age = media.Movie('Ice Age',
+                      'A movie about animals protecting a child',
                       'https://images-na.ssl-images-amazon.com/images/M/MV5BMTEwMzU0NTEyNTVeQTJeQWpwZ15BbWU2MDIwNzA3OQ@@._V1_.jpg',
                       'https://www.youtube.com/watch?v=cMfeWyVBidk')
 
-avatar = media.Movie('Avatar', 'When a marine is on an alien planet',
+avatar = media.Movie('Avatar',
+                     'When a marine is on an alien planet',
                      'https://glennewalker.files.wordpress.com/2009/12/c8d13-avatar.jpg',
                      'https://www.youtube.com/watch?v=5PSNL1qE6VY')
 
 midnight_in_paris = media.Movie('Midnight in Paris',
-                                'A movie about a screenwriter, who travels back in time at midnight.',
+                                'A movie about a screenwriter,'
+                                ' who travels back in time at midnight.',
                                 'https://natashastander.files.wordpress.com/2014/07/mip.jpg',
                                 'https://www.youtube.com/watch?v=FAfR8omt-CY')
 
-school_of_rock = media.Movie('School of Rock', 'A movie about using rock music to learn',
+school_of_rock = media.Movie('School of Rock',
+                             'A movie about using rock music to learn',
                              'http://www.gstatic.com/tv/thumb/movieposters/33094/p33094_p_v8_aa.jpg',
                              'https://www.youtube.com/watch?v=-_UedgKyL1o')
 
-ratatouille = media.Movie('Ratatouille', 'A Rat is a chef in Paris',
+ratatouille = media.Movie('Ratatouille',
+                          'A Rat is a chef in Paris',
                           'http://www.gstatic.com/tv/thumb/dvdboxart/165058/p165058_d_v8_aa.jpg',
                           'https://www.youtube.com/watch?v=3YG4h5GbTqU')
 
-movies = [toy_story, ice_age, avatar, midnight_in_paris, school_of_rock, ratatouille]
+movies = [toy_story, ice_age, avatar, midnight_in_paris, school_of_rock,
+          ratatouille]
 
-# Calling the function from fresh_tomatoes to create a html page and display the movies.
+# Calling the function from fresh_tomatoes to create a html page
+# and display the movies.
 
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -2,11 +2,18 @@ import webbrowser
 
 
 class Movie:
+    """
+    This class containes the attibutes which will be used to display the movies with the trailer.
+    """
     def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        """
+        :param movie_title: The title of the movie.
+        :param movie_storyline: The story of the movie.
+        :param poster_image: The poster image url of the movie.
+        :param trailer_youtube:The youtube trailer url of the movie.
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    def play_trailer(self):
-        webbrowser.open(self.trailer_youtube_url)
