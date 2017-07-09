1cba0b20a3bed8bb5782588ff86689a54f562c36
Vishesh <vmath89@gmail.com>
2017-07-09 14:24:01 +0200

    formatting using autopep8

--- media.py
+++ media.py
@@ -1,11 +1,14 @@
-import webbrowser
-
-
 class Movie:
     """
     This class containes the attibutes which will be used to display the movies with the trailer.
     """
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+
+    def __init__(
+            self,
+            movie_title,
+            movie_storyline,
+            poster_image,
+            trailer_youtube):
         """
         :param movie_title: The title of the movie.
         :param movie_storyline: The story of the movie.
@@ -16,4 +19,3 @@ class Movie:
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
-
